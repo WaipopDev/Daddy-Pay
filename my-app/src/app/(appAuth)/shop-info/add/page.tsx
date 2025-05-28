33ee27a90fcb938bb2af9217390b953ae1a55898
Waipop B <>
2025-05-28 18:13:13 +0700

    no message

--- my-app/src/app/(appAuth)/shop-info/add/page.tsx
+++ my-app/src/app/(appAuth)/shop-info/add/page.tsx
@@ -1,32 +1,31 @@
 'use client';
 import React from "react";
-import PageNoData from "@/components/PageNoData";
 import { useAppSelector } from "@/store/hook";
 import { Button, Col } from "react-bootstrap";
 import { useRouter } from "next/navigation";
+import ShopInfoFrom from "@/components/ShopInfoFrom/ShopInfoFrom";
 
-const ShopInfoPage = () => {
+const ShopInfoAddPage = () => {
     const lang = useAppSelector(state => state.lang) as { [key: string]: string }
     const router = useRouter();
 
     const handleAddShop = () => {
-        // Logic to add a new shop
-        console.log("Add new shop clicked");
-        router.push('/shop-info/add');
+    };
+
+    const handleCancel = () => {
+        router.push('/shop-info');
     };
     return (
         <main className="bg-white p-3">
             <div className="flex border-b border-gray-300 pb-3">
-                <Col className="flex justify-start">
-
-                </Col>
                 <Col className="flex justify-end">
-                    <Button variant="primary" onClick={() => handleAddShop()}><i className="fa-solid fa-plus pr-2"></i>{lang['button_add_shop']}</Button>
+                    <Button variant="primary" onClick={() => handleAddShop()}><i className="fa-solid fa-floppy-disk pr-2"></i>{lang['button_save']}</Button>
+                    <Button variant="secondary" onClick={() => handleCancel()} className="ml-2"><i className="fa-solid fa-xmark pr-2"></i>{lang['button_cancel']}</Button>
                 </Col>
             </div>
-            <PageNoData />
+            <ShopInfoFrom />
         </main>
     )
 }
 
-export default ShopInfoPage
+export default ShopInfoAddPage
